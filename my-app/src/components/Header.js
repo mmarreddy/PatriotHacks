70df4203ace550dba3fceb001a7cd863f90c73be
Mounika Marreddy <mouni@mounis-macbook.mynetworksettings.com>
2024-10-15 21:51:00 -0400

    removedLogo

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -2,15 +2,12 @@ import React from 'react'
 import "../index"
 import{Link} from "react-router-dom"
 import '../index.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouseChimney } from '@fortawesome/free-solid-svg-icons'; 
 
 const Header = () => {
   return (
     <div className='header'>
       <div className='header-item'>
       <h1 id='logo'>Buyers Guide</h1>
-      <FontAwesomeIcon icon={faHouseChimney} size="2x" style={{color:'f5d7e1'}} />
       </div>
         <div className='header-item'>
         <nav className='nav'>
